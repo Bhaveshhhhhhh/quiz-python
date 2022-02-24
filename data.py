ac32121a01f0454c22119702b31e6937d1935572
Bhavesh Choudhary <92804250+Bhaveshhhhhhh@users.noreply.github.com>
2022-02-24 15:04:52 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,6 @@
+question_data = [
+    {"text": "A slag's blood is green", "answer": "True"},
+    {"text": "Prince Harry is taller than Prince William", "answer": "False"},
+    {"text": "Waterloo has the greatest number of tube platforms in London", "answer": "True"},
+    {"text": "my name is Bhaveeesh", "answer": "True"},
+]
---
+++ mainquiz.py
@@ -0,0 +1,18 @@
+from quiz import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+
+
+print("quiz completed")
+print(f"your final score is :4/{len(question_bank)}")
